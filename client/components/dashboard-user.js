0a4f370d08164aedaef0eac40cbe72428f690508
Meder <metaa@me.com>
2020-05-26 21:38:16 +0600

    updated homework 11-2 again

--- client/components/dashboard-user.js
+++ client/components/dashboard-user.js
@@ -5,7 +5,7 @@ const DashboardUser = () => {
   const { user } = useParams()
   return (
     <div>
-      <div id="title"> Main </div>
+      <div id="title">Profile</div>
       <Link to="/dashboard"> Go To Root </Link>
       <Link to="/dashboard/main"> Go To Main </Link>
       <div id="username"> {user} </div>
